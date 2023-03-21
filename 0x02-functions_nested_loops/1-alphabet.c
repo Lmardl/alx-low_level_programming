0746935556a8488d4c09dfea9be09b905dae4b4c
Lmardl <elhajouiimran@gmail.com>
2023-03-21 09:47:30 -0700

    nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet - print all alphabet in lowercase
+ * print_alphabet - print all alphabet in lowercase.
  *
- * Return : Always 0 (Success)
+ * Return:Always 0(Success)
  */
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
