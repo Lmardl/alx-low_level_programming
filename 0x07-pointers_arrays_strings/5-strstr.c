c50f6af2435e9beaf3ca42d29a1059acf315a2a9
Lmardl <elhajouiimran@gmail.com>
2023-04-03 16:20:34 -0700

    0x07

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,17 +7,20 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	for(;*haystack!='\0';haystack++)
+	for (; *haystack != '\0'; haystack++)
 	{
-		char*l=haystack;
-		char*p= needle;
-		while (*l==*p && *p!='\0')
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
 		{
 			l++;
 			p++;
 		}
-		if(*p=='\0')
+
+		if (*p == '\0')
 			return (haystack);
 	}
+
 	return (0);
 }
