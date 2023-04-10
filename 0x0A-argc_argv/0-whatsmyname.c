f2dc2dc051dd096b96c7db86ead19dbabeaeb75f
Lmardl <elhajouiimran@gmail.com>
2023-04-10 07:37:37 -0700

    first

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program.
+ * @argc: counts the number of arguments.
+ * @argv: prints the string of arguments.
+ * Return: 0.
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("%s\n",*argv);
+
+	return (0);
+}
